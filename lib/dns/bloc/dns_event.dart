ddcc3ae74ef43323aac95efab4572d9edbc8b080
Stepler <stephankondratev@gmail.com>
2022-03-15 18:28:14 +0300

    need to understand bloc

--- lib/dns/bloc/dns_event.dart
+++ lib/dns/bloc/dns_event.dart
@@ -1,5 +1,29 @@
 abstract class DnsEvent{
   const DnsEvent();
 
+  const factory DnsEvent.startSearch() = _Start;
 
+  const factory DnsEvent.uplDevices() = _Upload;
+
+  const factory DnsEvent.delDdevices() = _Delete;
+
+  const factory DnsEvent.stopSearch() = _Stop;
+
+  R map<R>({
+    required R Function(_Start value) startSearch,
+    required R Function(_Upload value) uplDevices,
+    required R Function(_Delete value) delDevices,
+    required R Function(_Stop value) stopSearch,
+  }){
+    final it =  this;
+    if (it is _Start){
+      return startSearch(it);
+    } else if (it is _Upload){
+      return uplDevices(it);
+    } else if (it is _Delete){
+      return delDevices(it);
+    } else if (it is _Stop){
+      return stopSearch(it);
+    }
+  }
 }
